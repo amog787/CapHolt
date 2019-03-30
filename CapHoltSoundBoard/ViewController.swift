e9d60cbbfa621f58547a981d647d6a9e982bff79
amog787 <amoghmaiya@gmail.com>
2019-03-30 17:38:07 +0530

    Some Moar stuff

--- CapHoltSoundBoard/ViewController.swift
+++ CapHoltSoundBoard/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
-    var sounds = ["one","two","three"]
+    var sounds = [NSString]() //["one","two","three"]
     
     func numberOfSections(in tableView: UITableView) -> Int {
         return 1
@@ -23,15 +23,33 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         let cell = tableView.dequeueReusableCell(withIdentifier: "holt", for: indexPath)
             as! HoltTableViewCell
         cell.num.text = "\(indexPath.row + 1)"
-        cell.name.text = sounds[indexPath.row]
+        cell.name.text = sounds[indexPath.row] as String
         return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        getSongs()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    func getSongs() {
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        
+        do {
+            let items : [NSString] = try fm.contentsOfDirectory(atPath: path) as [NSString]
+            for item in items{
+                if item.pathExtension == "m4a"{
+                    sounds.append(item.deletingPathExtension as NSString)
+                }
+            }
+        } catch{
+            print(error.localizedDescription)
+        }
 }
-
+}
